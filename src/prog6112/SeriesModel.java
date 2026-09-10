abe34969429f1e191cf93945592f61646b101afe
MalebanaGolekane <malebanatekano@gmail.com>
2026-09-10 21:26:46 +0200

    Add Series model class

---
+++ src/prog6112/SeriesModel.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package prog6112;
+
+/**
+ *
+ * @author Tekano Malebana
+ */
+public class SeriesModel {
+
+    public String Seriesid;
+    public String SeriesName;
+    public String SeriesAge;
+    public String SeriesNumberOfEpisodes;
+}
